ce002c923a95ed8a74201c828728dda2374b6ee1
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-08-03 08:32:49 -0300

    Fixes initial position on right half.

--- binary_search/python/1/binary_search.py
+++ binary_search/python/1/binary_search.py
@@ -2,11 +2,6 @@ def binary_search(target, arr, init_pos=0):
     if len(arr) == 0:
         return -1
 
-    if len(arr) == 1:
-        if arr[0] == target:
-            return init_pos
-        return -1
-
     m = len(arr) // 2
     if arr[m] == target:
         return m+init_pos
@@ -14,4 +9,4 @@ def binary_search(target, arr, init_pos=0):
     if arr[m] > target:
         return binary_search(target, arr[:m], 0)
 
-    return binary_search(target, arr[m:], m)
+    return binary_search(target, arr[m+1:], m+1)
